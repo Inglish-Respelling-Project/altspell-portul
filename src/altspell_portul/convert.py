7c801edfa22d73a5e47443722b9bb6a5e53ac9bd
Nicholas Johnson <njohnson@posteo.net>
2025-01-02 00:00:00 +0000

    Put fwd/rev Converters in separate classes

--- src/altspell_portul/convert.py
+++ src/altspell_portul/convert.py
@@ -22,26 +22,37 @@ from . import data
 import spacy
 
 
-class Dictionary:
-    def __init__(self, fwd: bool):
+class FwdDictionary:
+    def __init__(self):
         self.dict = {}
-        self._populate_dict(fwd)
+        self._populate_dict()
 
-    def _populate_dict(self, fwd: bool):
+    def _populate_dict(self):
         with files(data).joinpath('portul-dict.csv').open('r') as file:
 
             for row in csv.reader(file):
                 tradspell = row[0]
                 portul = row[1]
 
-                if fwd:
-                    pos = row[3]
-                    if pos == '':
-                        pos = None
+                pos = row[3]
+                if pos == '':
+                    pos = None
 
-                    self.dict[(tradspell, pos)] = portul
-                else:
-                    self.dict[portul] = tradspell
+                self.dict[(tradspell, pos)] = portul
+
+class RevDictionary:
+    def __init__(self):
+        self.dict = {}
+        self._populate_dict()
+
+    def _populate_dict(self):
+        with files(data).joinpath('portul-dict.csv').open('r') as file:
+
+            for row in csv.reader(file):
+                tradspell = row[0]
+                portul = row[1]
+
+                self.dict[portul] = tradspell
 
 class Converter:
     try:
@@ -52,9 +63,8 @@ class Converter:
         download('en_core_web_sm')
         _nlp = spacy.load('en_core_web_sm', enable=['tokenizer', 'tagger'])
 
-    def __init__(self, fwd: bool):
-        self._fwd = fwd
-        self._dict = Dictionary(self._fwd)
+class FwdConverter(Converter):
+    _dict = FwdDictionary()
 
     def convert_para(self, text: str) -> str:
         out_tokens = []
@@ -63,54 +73,67 @@ class Converter:
         for token in doc:
             token_lower = token.text.lower()
 
-            if self._fwd:
-                pos = token.pos_
-                match pos:
-                    case 'NOUN':
-                        pos = 'n'
-                    case 'VERB':
-                        pos = 'v'
-                    case 'ADJ':
-                        pos = 'adj'
-                    case 'ADV':
-                        pos = 'adv'
-                    case 'INTJ':
-                        pos = 'interj'
-                    case _:
-                        pos = None
+            pos = token.pos_
+            match pos:
+                case 'NOUN':
+                    pos = 'n'
+                case 'VERB':
+                    pos = 'v'
+                case 'ADJ':
+                    pos = 'adj'
+                case 'ADV':
+                    pos = 'adv'
+                case 'INTJ':
+                    pos = 'interj'
+                case _:
+                    pos = None
 
-                if (token_lower, None) in self._dict.dict:
-                    if token.text[0].isupper():
-                        word = self._dict.dict[(token_lower, None)]
-                        word = word[0].upper() + word[1:]
-                        out_tokens.append(word)
-                    else:
-                        out_tokens.append(self._dict.dict[(token_lower, None)])
-                elif (token_lower, pos) in self._dict.dict:
-                    if token.text[0].isupper():
-                        word = self._dict.dict[(token_lower, pos)]
-                        word = word[0].upper() + word[1:]
-                        out_tokens.append(word)
-                    else:
-                        out_tokens.append(self._dict.dict[(token_lower, pos)])
-                elif (token.text, None) in self._dict.dict:
-                    out_tokens.append(self._dict.dict[(token.text, None)])
-                elif (token.text, pos) in self._dict.dict:
-                    out_tokens.append(self._dict.dict[(token.text, pos)])
+            if (token_lower, None) in self._dict.dict:
+                if token.text[0].isupper():
+                    word = self._dict.dict[(token_lower, None)]
+                    word = word[0].upper() + word[1:]
+                    out_tokens.append(word)
                 else:
-                    out_tokens.append(token.text)
+                    out_tokens.append(self._dict.dict[(token_lower, None)])
+            elif (token_lower, pos) in self._dict.dict:
+                if token.text[0].isupper():
+                    word = self._dict.dict[(token_lower, pos)]
+                    word = word[0].upper() + word[1:]
+                    out_tokens.append(word)
+                else:
+                    out_tokens.append(self._dict.dict[(token_lower, pos)])
+            elif (token.text, None) in self._dict.dict:
+                out_tokens.append(self._dict.dict[(token.text, None)])
+            elif (token.text, pos) in self._dict.dict:
+                out_tokens.append(self._dict.dict[(token.text, pos)])
             else:
-                if token_lower in self._dict.dict:
-                    if token.text[0].isupper():
-                        word = self._dict.dict[token_lower]
-                        word = word[0].upper() + word[1:]
-                        out_tokens.append(word)
-                    else:
-                        out_tokens.append(self._dict.dict[token_lower])
-                elif token.text in self._dict.dict:
-                    out_tokens.append(self._dict.dict[token.text])
+                out_tokens.append(token.text)
+
+            out_tokens.append(token.whitespace_)
+
+        return ''.join(out_tokens)
+
+class RevConverter(Converter):
+    _dict = RevDictionary()
+
+    def convert_para(self, text: str) -> str:
+        out_tokens = []
+
+        doc = Converter._nlp(text)
+        for token in doc:
+            token_lower = token.text.lower()
+
+            if token_lower in self._dict.dict:
+                if token.text[0].isupper():
+                    word = self._dict.dict[token_lower]
+                    word = word[0].upper() + word[1:]
+                    out_tokens.append(word)
                 else:
-                    out_tokens.append(token.text)
+                    out_tokens.append(self._dict.dict[token_lower])
+            elif token.text in self._dict.dict:
+                out_tokens.append(self._dict.dict[token.text])
+            else:
+                out_tokens.append(token.text)
 
             out_tokens.append(token.whitespace_)
 
