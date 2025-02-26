2b1c1804eaa45e746e72240fb9481a90bdc8eeba
Nicholas Johnson <njohnson@posteo.net>
2025-02-26 00:00:00 +0000

    Rewrite to conform to new plugin interface

--- src/altspell_portul/__init__.py
+++ src/altspell_portul/__init__.py
@@ -16,17 +16,17 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-from altspell.plugin import PluginBase
-from .convert import FwdConverter, RevConverter
+from altspell_plugins import PluginBase
+from .translate import FwdTranslator, RevTranslator
 
 
 class Plugin(PluginBase):
     def __init__(self):
-        self._fwd_converter = FwdConverter()
-        self._rev_converter = RevConverter()
+        self._fwd_translator = FwdTranslator()
+        self._rev_translator = RevTranslator()
 
-    def convert_to_altspell(self, tradspell_text: str) -> str:
-        return self._fwd_converter.convert_para(tradspell_text)
+    def translate_to_respelling(self, traditional_text: str) -> str:
+        return self._fwd_converter.convert_para(traditional_text)
 
-    def convert_to_tradspell(self, altspell_text: str) -> str:
-        return self._rev_converter.convert_para(altspell_text)
+    def translate_to_traditional_spelling(self, respelled_text: str) -> str:
+        return self._rev_converter.convert_para(respelled_text)
